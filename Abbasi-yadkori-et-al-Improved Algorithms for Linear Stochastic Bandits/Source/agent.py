cf847be24857a057fec6c4008fc8fb228c54009a
LI ZITIAN <e0732494@u.nus.edu>
2023-02-10 16:58:41 +0800

    Update the source file of the OFUL agent and the environment

--- Abbasi-yadkori-et-al-Improved Algorithms for Linear Stochastic Bandits/Source/agent.py
+++ Abbasi-yadkori-et-al-Improved Algorithms for Linear Stochastic Bandits/Source/agent.py
@@ -1,6 +1,9 @@
+from typing import Union
+
 import numpy as np
 from numpy import linalg
 from copy import deepcopy
+from scipy.optimize import minimize
 
 
 def get_factorization(matrix: np.ndarray):
@@ -41,7 +44,35 @@ def get_factorization(matrix: np.ndarray):
         U[1][1] = -1
         U[0:, 1] = U[0:, 1] / np.sqrt(U[0][1] ** 2 + U[1][1] ** 2)
 
+        assert np.max(np.abs(U @ np.array([[lambda1, 0.0], [0.0, lambda2]]) @ U.T - matrix)) < 1e-6, "fail to factorize"
+
         return lambda1, lambda2, U
+
+
+def get_xy(lambda1: float, lambda2: float, U: np.ndarray, x0: np.ndarray, beta: float, t: Union[float, np.ndarray]):
+    """use the return value of function get_factorization to calculate the position of point
+    Equation of ellipsoid (x-x_0)^TU[[lambda_1, 0], [0, lambda2]]U^T(x-x0)=beta
+
+    Args:
+        lambda1 (float): bigger eigen value of the matrix
+        lambda2 (float): smaller eigen value of the matrix
+        U (np.ndarray): the unitary matrix
+        x0 (np.ndarray): the center of ellipsoid
+        beta (np.ndarray): the number
+        t (Union[float, np.ndarray]): the parameter of the point
+
+    Returns:
+        x(np.ndarray): each column of x represents a point in the 2-d space
+    """
+    x1 = np.expand_dims(np.sqrt(beta) / np.sqrt(lambda1) * np.sin(t), axis=0)
+    x2 = np.expand_dims(np.sqrt(beta) / np.sqrt(lambda2) * np.cos(t), axis=0)
+    x = np.concatenate([x1, x2], axis=0)
+
+    if type(t) == float:
+        x = U @ x + x0
+    else:
+        x = U @ x + x0[:, np.newaxis]
+    return x
 
 
 class ConfidenceBall_Agent(object):
@@ -50,13 +81,7 @@ class ConfidenceBall_Agent(object):
 
 
 class OFUL_Agent(object):
-    def __init__(
-        self,
-        R: float,
-        lambda_: float,
-        L: float,
-        S: float,
-    ) -> None:
+    def __init__(self, R: float, lambda_: float, L: float, S: float, delta: float) -> None:
         """Initialization of OFUL algorithm,
         O: Optimism, U: Uncertainty, F: Face, L: Linear
         We assume the action space is always the unit ball in 2-d space
@@ -64,81 +89,126 @@ class OFUL_Agent(object):
         Args:
             R (float): The nois is conditionally R-subgaussian random variable
             _lambda (float): Regularization coefficient
-            L (float): \|X_t\|_2 \le L
-            S (float): \|\theta_*\|_2 \le S
+            L (float): $\|X_t\|_2 \le L$
+            S (float): $\|\theta_*\|_2 \le S$
+            delta (float): tolerance of failure probability
         """
-        self.d = d
+        self.d = 2
         self.R = R
         self.lambda_ = lambda_
         self.L = L
         self.S = S
+        self.delta = delta
 
-        self.V = np.eye(d) * lambda_
         self.H_action = list()  # history of reward
         self.H_reward = list()  # history of action
+
+        # $\hat{\theta}_t = A_t^{-1} B_t$
+        self.V_t = lambda_ * np.eye(self.d)  # $A_t=\sum_{s=1}^t x_sx_s^T+\lambda I$
+        self.Y_t = np.zeros(2)  # $B_t=\sum_{s=1}^t y_sx_s$
 
         self.t = 0
 
     def action(self):
-        theta = self.get_theta()
-        theta_l2 = linalg.norm(theta, ord=2)
-        if np.abs(theta_l2) < 1e-4:
+        tilde_theta = self.get_theta()
+        tilde_theta_l2 = linalg.norm(tilde_theta, ord=2)
+        if np.abs(tilde_theta) < 1e-6:
             act = np.zeros(self.d)
         else:
-            act = theta / theta_l2
+            act = tilde_theta / tilde_theta_l2
+        self.H_action.append(act)
         return act
 
     def update(self, reward):
+        action = self.H_action[self.t]
+        self.V_t = self.V_t + action[:, np.newaxis] @ action[np.newaxis, :]
+        self.Y_t = self.Y_t + action * reward
         self.t += 1
 
     def get_theta(self):
         # solve the problem
         # \tilde{\theta}_t=\arg\max_{\theta\in C_{t-1}}\|\theta\|_2
-        return np.zeros(self.d)
 
-    def get_beta(self):
-        # calculate the beta_t
-        pass
+        # calculate the $C_{t-1}$, including $V_{t-1}$, $\beta_{t-1}$, $\hat{\theta}$
+        theta_hat_t = np.linalg.solve(self.V_t, self.Y_t)
+        beta_t = self.R * np.sqrt(self.d * np.log((1 + t * self.L**2 / self.lambda_) / self.delta) + np.sqrt(self.lambda_) * self.S)
+        beta_t = beta_t**2
+
+        # find the point with maximum l2 norm
+        ## first step, grid search
+        lambda1, lambda2, U = get_factorization(self.V_t)
+        t = np.linspace(0, np.pi * 2, 10)
+        x1 = np.expand_dims(np.sqrt(beta_t) / np.sqrt(lambda1) * np.sin(t), axis=0)
+        x2 = np.expand_dims(np.sqrt(beta_t) / np.sqrt(lambda2) * np.cos(t), axis=0)
+        x = np.concatenate([x1, x2], axis=0)
+        x = U @ x + theta_hat_t[:, np.newaxis]
+        x_l2norm = np.linalg.norm(x, axis=0)
+        maxindex = np.argmax(x_l2norm)
+
+        ## second step, use the result from gird search as the initial point of optimizer
+        f = lambda t: -np.linalg.norm(get_xy(lambda1=lambda1, lambda2=lambda2, U=U, x0=theta_hat_t, beta=beta_t, t=t))
+        res = minimize(fun=f, x0=maxindex)
+        maxindex_scipy = res.x[0]
+        tilde_theta = U @ np.array(get_xy(lambda1=lambda1, lambda2=lambda2, U=U, x0=theta_hat_t, beta=beta_t, t=maxindex_scipy))
+
+        return tilde_theta
 
 
-# unit test 1, test the algorithm that can find the point
-## visualize a ellipsoid, $(x-x0)^TA(x-x0)=\beta$
-import matplotlib.pyplot as plt
-from scipy.optimize import minimize
+#%% unit test 1, test the algorithm that can find the point
+# ## visualize a ellipsoid, $(x-x0)^TA(x-x0)=\beta$
+# import matplotlib.pyplot as plt
+# from scipy.optimize import minimize
 
-d = 2
-np.random.seed(12345)
-A = np.random.uniform(size=(d, d))
-A = A @ A.T
-# A = np.array([[3.0, 1.0], [1.0, 1.0]])
-lambda1, lambda2, U = get_factorization(A)
-# print(P.T @ J @ P)
-print(A)
-print(lambda1, lambda2)
-print(U @ np.array([[lambda1, 0.0], [0.0, lambda2]]) @ U.T)
-x0 = np.random.uniform(size=(d)) * 10
-beta = np.random.uniform(low=0.0, high=1.0)
+# d = 2
+# np.random.seed(12345)
+# # A = np.random.uniform(size=(d, d))
+# # A = A @ A.T
+# A = np.array(
+#     [
+#         [
+#             1.0 / 36.0,
+#             0.0,
+#         ],
+#         [0.0, 1.0],
+#     ]
+# )
+# # A = np.array([[3.0, 1.0], [1.0, 1.0]])
+# lambda1, lambda2, U = get_factorization(A)
+# # print(P.T @ J @ P)
+# print(A)
+# print(lambda1, lambda2)
+# print(U @ np.array([[lambda1, 0.0], [0.0, lambda2]]) @ U.T)
+# # x0 = np.random.uniform(size=(d)) * 10
+# x0 = np.array([0.0, 10.0])
+# # beta = np.random.uniform(low=0.0, high=1.0)
+# beta = 1.0
 
-t = np.linspace(0, np.pi * 2, 10)
-x1 = np.expand_dims(np.sqrt(beta) / np.sqrt(lambda1) * np.sin(t), axis=0)
-x2 = np.expand_dims(np.sqrt(beta) / np.sqrt(lambda2) * np.cos(t), axis=0)
-np.newaxis
-x = np.concatenate([x1, x2], axis=0)
-x = U @ x + x0[:, np.newaxis]
+# t = np.linspace(0, np.pi * 2, 10)
+# x1 = np.expand_dims(np.sqrt(beta) / np.sqrt(lambda1) * np.sin(t), axis=0)
+# x2 = np.expand_dims(np.sqrt(beta) / np.sqrt(lambda2) * np.cos(t), axis=0)
+# # np.newaxis
+# x = np.concatenate([x1, x2], axis=0)
+# x = U @ x + x0[:, np.newaxis]
 
-plt.figure()
-plt.plot(x[0, :], x[1, :])
-l2norm = np.linalg.norm(x, axis=0)
-maxpoint = np.argmax(l2norm)
-plt.scatter(x[0, maxpoint], x[1, maxpoint])
-plt.xlim((-15, 15))
-plt.ylim((-15, 15))
-plt.grid(True, which="both")
+# plt.figure()
+# plt.plot(x[0, :], x[1, :])
+# l2norm = np.linalg.norm(x, axis=0)
+# maxpoint = np.argmax(l2norm)
+# plt.scatter(x[0, maxpoint], x[1, maxpoint])
+# plt.xlim((-15, 15))
+# plt.ylim((-15, 15))
+# plt.grid(True, which="both")
 
-f = lambda t: -np.linalg.norm(U @ np.array([np.sqrt(beta) / np.sqrt(lambda1) * np.sin(t), np.sqrt(beta) / np.sqrt(lambda2) * np.cos(t)]) + x0)
-res = minimize(fun=f, x0=maxpoint)
-maxpoint_scipy = res.x[0]
-point = U @ np.array([np.sqrt(beta) / np.sqrt(lambda1) * np.sin(maxpoint_scipy), np.sqrt(beta) / np.sqrt(lambda2) * np.cos(maxpoint_scipy)]) + x0
-plt.scatter(point[0], point[1], c="red")
+# f = lambda t: -np.linalg.norm(U @ np.array([np.sqrt(beta) / np.sqrt(lambda1) * np.sin(t), np.sqrt(beta) / np.sqrt(lambda2) * np.cos(t)]) + x0)
+# res = minimize(fun=f, x0=maxpoint)
+# maxpoint_scipy = res.x[0]
+# point = U @ np.array([np.sqrt(beta) / np.sqrt(lambda1) * np.sin(maxpoint_scipy), np.sqrt(beta) / np.sqrt(lambda2) * np.cos(maxpoint_scipy)]) + x0
+# plt.scatter(point[0], point[1], c="red")
+# print(point)
+# plt.show()
 
-plt.show()
+#%% unit test 2
+# v = np.array([2.0, 1.0])
+# print(v[:, np.newaxis] @ v[np.newaxis, :])
+
+#%% unit test 3
--- Abbasi-yadkori-et-al-Improved Algorithms for Linear Stochastic Bandits/Source/env.py
+++ Abbasi-yadkori-et-al-Improved Algorithms for Linear Stochastic Bandits/Source/env.py
@@ -1,1 +1,34 @@
+from typing import Union
 import numpy as np
+
+
+class Env_FixedActionSpace(object):
+    def __init__(self, theta: Union[None, np.ndarray], d: int = 2, random_seed: int = 12345, R: float = 0.1) -> None:
+        """The environment that always adopts fixed action space, which is a unit ball in the d-dimension space
+
+        Args:
+            theta (np.ndarray): The unknown theta of the environment
+            d (int, optional): Dimension of the action space. Defaults to 2.
+            random_seed (int, optional): The random seed. Defaults to 12345.
+            R (float, optional): The noise is R-subgaussian random variable
+        """
+        self.d = d
+        self.random_seed = random_seed
+        self.R = R
+        self.random_generator = np.random.Generator(random_seed)
+        if theta is not None:
+            assert theta.shape[0] == d, "The size of theta doesn't match"
+            self.theta = theta
+        else:
+            self.theta = self.random_generator.uniform(low=0.0, high=1.0, size=2)
+            self.theta = self.theta / np.linalg.norm(self.theta)
+
+    def response(self, action: np.ndarray):
+        """Given the action of the agent, generate the reward
+
+        Args:
+            action (np.ndarray): the vector of action
+        """
+        assert action.shape[0] == self.d and len(action.shape) == 1, "The action doesn't match the dimension"
+        reward = action @ self.theta + self.random_generator.normal(loc=0.0, scale=self.R)
+        return reward
